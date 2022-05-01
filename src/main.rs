a101dc5695ab63a3e5a8813b472dacc0cc5aac18
adust09 <shoukitsuda@icloud.com>
2022-05-02 07:04:57 +0900

    add .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+pub mod assembler;
 pub mod instruction;
 pub mod repl;
 pub mod vm;
 
+#[macro_use]
 fn main() {
     let mut repl = repl::REPL::new();
     repl.run();
+
+    extern crate nom;
 }
