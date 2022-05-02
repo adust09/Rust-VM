86f55b515bd0f4345fdaf69970c486a130bb173b
adust09 <shoukitsuda@icloud.com>
2022-05-02 15:41:43 +0900

    Podsフォルダをgit管理対象から除外

--- target/rls/debug/build/nom-5b482c5d81460752/build_script_build-5b482c5d81460752.d
+++
@@ -1,5 +0,0 @@
-/Users/ts21/rust-vm/target/rls/debug/build/nom-5b482c5d81460752/build_script_build-5b482c5d81460752: /Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/nom-4.2.3/build.rs
-
-/Users/ts21/rust-vm/target/rls/debug/build/nom-5b482c5d81460752/build_script_build-5b482c5d81460752.d: /Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/nom-4.2.3/build.rs
-
-/Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/nom-4.2.3/build.rs:
--- target/rls/debug/deps/version_check-5f2793e39d7b7af7.d
+++
@@ -1,7 +0,0 @@
-/Users/ts21/rust-vm/target/rls/debug/deps/version_check-5f2793e39d7b7af7.rmeta: /Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs
-
-/Users/ts21/rust-vm/target/rls/debug/deps/libversion_check-5f2793e39d7b7af7.rlib: /Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs
-
-/Users/ts21/rust-vm/target/rls/debug/deps/version_check-5f2793e39d7b7af7.d: /Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs
-
-/Users/ts21/.cargo/registry/src/github.com-1ecc6299db9ec823/version_check-0.1.5/src/lib.rs:
